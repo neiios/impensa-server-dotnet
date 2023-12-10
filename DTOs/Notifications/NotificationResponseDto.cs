2ea145c433a64f59c114268e0d146484d2f07822
tmneth <80415416+tmneth@users.noreply.github.com>
2023-12-10 13:46:11 +0200

    Declare notification properties as nullable

--- DTOs/Notifications/NotificationResponseDto.cs
+++ DTOs/Notifications/NotificationResponseDto.cs
@@ -4,11 +4,11 @@ public class NotificationResponseDto
 {
     public Guid Id { get; set; }
 
-    public DateTime CreatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
 
-    public string Title { get; set; }
+    public string? Title { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     public bool IsRead { get; set; } 
 }
--- Models/Notification.cs
+++ Models/Notification.cs
@@ -5,8 +5,8 @@ public class Notification
     public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public Guid UserId { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public bool IsRead {get; set; }
-    public string Description { get; set; }
+    public string?1 Description { get; set; }
     public required User User { get; set; }
 }
